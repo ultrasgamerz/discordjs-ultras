ae3e387be2f795f7fe512efa103828a9b4c1d320
ultrasgamerz <85828760+ultrasgamerz@users.noreply.github.com>
2021-08-28 07:19:21 +0800

    Add files via upload

---
+++ commands/moderation/setwelcome.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js")
+const db = require("quick.db")
+
+module.exports = {
+  name: "setwelcome",
+  category: "moderation",
+  usage: "setwelcome <#channel>",
+  description: "Set the welcome channel",
+  run: (client, message, args) => {
+    
+    let channel = message.mentions.channels.first()
+    
+    if(!channel) {
+      return message.channel.send("Please Mention the channel first")
+    }
+    
+    db.set(`welchannel_${message.guild.id}`, channel.id)
+    
+    message.channel.send(`Welcome Channel is seted as ${channel}`)
+  }
+}
